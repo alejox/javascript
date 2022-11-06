cffa07f48f427f215663e54a8d40b9656aaa1f98
alejox <alejoxdrums@gmail.com>
2022-11-06 17:47:56 -0500

    updates

---
+++ 09-Funciones/js/11-app.js
@@ -0,0 +1,9 @@
+/* const aprendiendo = function(nombre, nombre2){
+  console.log(`Aprendiendo ${nombre} y ${nombre2}`);
+}
+
+aprendiendo('Javascript', 'CSS') */
+
+const aprendiendo2 = (tecnologia, tecnologia2) => `Aprendiendo ${tecnologia} y ${tecnologia2}`
+
+console.log(aprendiendo2('Javascript', 'CSS'));
